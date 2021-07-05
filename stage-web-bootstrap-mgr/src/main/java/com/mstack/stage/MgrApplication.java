51cc9d0fbe0fae8645f448503ae42df39b022696
limeng429 <limeng429@jd.com>
2021-07-05 20:14:47 +0800

    FEA:console

---
+++ stage-web-bootstrap-mgr/src/main/java/com/mstack/stage/MgrApplication.java
@@ -0,0 +1,20 @@
+package com.mstack.stage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class MgrApplication {
+
+    public static void main(String[] args) {
+        log.info("MgrApplication begin...");
+        try {
+            SpringApplication.run(MgrApplication.class, args);
+            log.info("MgrApplication run sucessful!");
+        } catch (Throwable throwable) {
+            log.error("MgrApplication run error!error msg is {}", throwable.getMessage(), throwable);
+        }
+    }
+}
